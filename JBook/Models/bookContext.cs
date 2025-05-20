350fabd7ccb5d1ba372d5e5945b248f68010bae5
24839 <24839@DESKTOP-37O5QAN>
2025-05-20 17:24:38 +0800

    添加项目文件。

---
+++ JBook/Models/bookContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JBook.Models
+{
+    public class bookContext : DbContext
+    {
+        public bookContext(DbContextOptions<bookContext> options)
+            : base(options)
+        { }
+        public DbSet<book> books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<book>().HasData(
+                                new book
+                                {
+                                    bookId = 1,
+                                    title = "Sun Zi's Art of War",
+                                    author = "Wu Sun",
+                                    ISBN = "9787552220094",
+                                },
+
+                                new book
+                                {
+                                    bookId = 2,
+                                    title = "Alive",
+                                    author = "Hua Yu",
+                                    ISBN = "9787530221532",
+                                }
+            );
+        }
+    }
+}
---
+++ JBook/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using JBook.Models;
+
+namespace JBook
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting(options => {
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = true;
+            });
+
+            services.AddControllersWithViews();
+
+            services.AddDbContext<bookContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("bookContext")));
+        }
+    }
+}
